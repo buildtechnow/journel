2b5265b9b86091ad1553c2c6eb6cff64d1e4aead
MeraRam <smskandaoff@gmail.com>
2024-09-28 19:14:05 +0530

    Api

---
+++ buildJournel/src/main/java/com/example/buildjournel/BuildJournelApplication.java
@@ -0,0 +1,13 @@
+package com.example.buildjournel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildJournelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuildJournelApplication.class, args);
+    }
+
+}
